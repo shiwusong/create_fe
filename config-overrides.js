2e8b649d9cf2a6b5a3614078cbae48029226eeca
shiwusong <shiwusong@bytedance.com>
2020-03-27 14:53:13 +0800

    feat: 移入public文件夹到client中

--- config-overrides.js
+++ config-overrides.js
@@ -3,8 +3,12 @@ const path = require('path')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const paths = require("react-scripts/config/paths");
-paths.appSrc = path.join(__dirname, '/client')
-paths.appIndexJs = path.join(paths.appSrc, '/entry.js')
+
+paths.appSrc = path.join(paths.appPath, 'client')
+paths.appPublic = path.join(paths.appSrc, 'public')
+paths.appHtml = path.join(paths.appPublic, 'index.html')
+paths.appIndexJs = path.join(paths.appSrc, 'entry.js')
+
 /* config-overrides.js */
 module.exports = function override(config, env) {
   const { ANALYZE } = process.env
